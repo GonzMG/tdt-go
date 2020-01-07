abde4e997d1ce2e4af2b8b9d5bb28605fc3f80e1
Gonzalo <gonzalo.molina1996@gmail.com>
2020-01-07 13:49:44 +0100

    Simple Table test created

---
+++ roman/roman_numerals_table_test.go
@@ -0,0 +1,27 @@
+package roman
+
+import "testing"
+
+var testCases = []struct {
+	// Atributes of the test
+	inputDecimal int
+	outputRoman  string
+}{
+	// Input and expected values of the test cases
+	{1, "I"},
+	{4, "IV"},
+	{6, "VI"},
+	{125, "CXXV"},
+	{47, "XLVII"},
+	{492, "CDXCII"},
+}
+
+func TestRomanToNumberTable(t *testing.T) {
+	for _, testCase := range testCases {
+		result := ConvertDecimalToRoman(testCase.inputDecimal)
+		if result != testCase.outputRoman {
+			t.Errorf("ERROR -> Input: %v | Output: %v | Expected: %v",
+				testCase.inputDecimal, result, testCase.outputRoman)
+		}
+	}
+}
