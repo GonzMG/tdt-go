89434c132b39de19b6886050380c1ca378e4f42b
Gonzalo <gonzalo.molina1996@gmail.com>
2020-01-07 14:39:23 +0100

    Sub table test done

--- roman/roman_numerals.go
+++ roman/roman_numerals.go
@@ -2,6 +2,8 @@ package roman
 
 import "errors"
 
+var errorNegativeNumber error = errors.New("The input can not be lower than 0")
+
 var decimalNumerals []int = []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
 var romanNumerals []string = []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC",
 	"C", "CD", "D", "CM", "M"}
@@ -10,7 +12,7 @@ func ConvertDecimalToRoman(decimalNumber int) (string, error) {
 	romanNumber := ""
 	lastIndex := 0
 	if decimalNumber < 0 {
-		return "", errors.New("The input can not be lower than 0")
+		return "", errorNegativeNumber
 	}
 	for index := range decimalNumerals {
 		if decimalNumber == decimalNumerals[index] {
--- roman/roman_numerals_table_test.go
+++ roman/roman_numerals_table_test.go
@@ -2,26 +2,52 @@ package roman
 
 import "testing"
 
-var testCases = []struct {
-	// Atributes of the test
-	inputDecimal int
-	outputRoman  string
-}{
-	// Input and expected values of the test cases
-	{1, "I"},
-	{4, "IV"},
-	{6, "VI"},
-	{125, "CXXV"},
-	{47, "XLVII"},
-	{492, "CDXCII"},
-}
-
 func TestRomanToNumberTable(t *testing.T) {
+	testCases := []struct {
+		// Atributes of the test
+		inputDecimal int
+		outputRoman  string
+	}{
+		// Input and expected values of the test cases
+		{1, "I"},
+		{4, "IV"},
+		{6, "VI"},
+		{125, "CXXV"},
+		{47, "XLVII"},
+		{492, "CDXCII"},
+	}
+
 	for _, testCase := range testCases {
 		result, _ := ConvertDecimalToRoman(testCase.inputDecimal)
 		if result != testCase.outputRoman {
 			t.Errorf("ERROR -> Input: %v | Output: %v | Expected: %v",
 				testCase.inputDecimal, result, testCase.outputRoman)
 		}
+	}
+}
+
+func TestRomanToNumberSubTable(t *testing.T) {
+	testCases := []struct {
+		// NOTE: we are going to group the input/outputs into negative
+		// and positive input cases.
+		nameTest     string
+		inputDecimal []int
+		outputRoman  []string
+		outputError  []error
+	}{
+		{"Testing positive numbers", []int{1, 125, 492}, []string{"I", "CXXV", "CDXCII"}, []error{nil, nil, nil}},
+		{"Testing negative numbers", []int{-1, -125}, []string{"", ""}, []error{errorNegativeNumber, errorNegativeNumber}},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.nameTest, func(t *testing.T) {
+			for i := range testCase.inputDecimal {
+				result, err := ConvertDecimalToRoman(testCase.inputDecimal[i])
+				if result != testCase.outputRoman[i] || err != testCase.outputError[i] {
+					t.Errorf("ERROR -> Input: %v | Output: %v | Expected: %v",
+						testCase.inputDecimal[i], result, testCase.outputRoman[i])
+				}
+			}
+		})
 	}
 }
